7dfeca344dbe3a0d733ae27636f2ee5c7f26ecc2
Lewis Njagi <lewismaybach@gmail.com>
2021-02-02 17:45:47 +0300

    final commit

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Quote } from './quote';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input() quote?: Quote;
 
+  
+  constructor(private elem:ElementRef) {
+    this.elem.nativeElement.style["boxShadow"] = "0 0 20px #000000";
+  }
 }
--- src/app/quotes-page/quotes-page.component.ts
+++ src/app/quotes-page/quotes-page.component.ts
@@ -10,15 +10,15 @@ export class QuotesPageComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(
       'Lewis',
-      'I am the light of the world',
+      'For God loved the world so much that he gave his one and only Son, so that everyone who believes in him will not perish but have eternal life. God sent his Son into the world not to judge the world, but to save the world through him.',
       'Jesus',
       new Date(2021, 1, 1)
     ),
     new Quote(
       'Lewis',
-      'I am the light of the world2',
+      'Love your enemies! Pray for those who persecute you! In that way, you will be acting as true children of your Father in heaven. For he gives his sunlight to both the evil and the good, and he sends rain on the just and the unjust alike.',
       'Jesus',
-      new Date(2021, 1, 1)
+      new Date(2021, 0, 31)
     ),
   ];
 
