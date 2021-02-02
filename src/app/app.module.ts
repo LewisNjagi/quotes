3215068ec3661791ac3e6793aa78c2b0aeb63504
Lewis Njagi <lewismaybach@gmail.com>
2021-02-02 15:55:52 +0300

    generate quote form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { QuotesPageComponent } from './quotes-page/quotes-page.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LandingPageComponent,
-    QuotesPageComponent
+    QuotesPageComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
